f79643b93abd52ade82ce8852492ba47033037b8
yuara <yu.arakawa112@gmail.com>
2020-08-20 22:38:45 +0900

    Made index page

--- markdownblog/config/urls.py
+++ markdownblog/config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.IndexView.as_view()),
     path("article/", include("blog.urls", namespace="article")),
 ]
