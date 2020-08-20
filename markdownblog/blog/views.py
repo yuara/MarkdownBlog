37e41bd34164924c57aad5e5bf451f7ef99347e3
yuara <yu.arakawa112@gmail.com>
2020-08-20 21:43:47 +0900

    Made ArticleDetailView

--- markdownblog/blog/views.py
+++ markdownblog/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Article
 
 # Create your views here.
+
+
+class ArticleDetailView(DetailView):
+    model = Article
